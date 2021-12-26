888c3ed204de0d97a60e67255304e09a7d466cfd
Gabriel Henrique <gabriel.hqs03@gmail.com>
2021-12-25 22:49:02 -0300

    Create get all todos by username

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -34,7 +42,9 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
